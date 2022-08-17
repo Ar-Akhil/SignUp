f9ca6a7542c71dbea60100e76dd242bf8eafd2cc
Ar-Akhil <ar.akhil1511@gmail.com>
2022-08-17 23:13:53 +0530

    added validity check with js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const emailInput = document.getElementById("form_email");
-console.log(emailInput.style.border);
+console.log("connected");
+const trail = document.getElementById("logoname");
+const button = document.getElementById("submitbutton");
+const number = document.getElementById("number");
+const password1 = document.getElementById("password");
+const password2 = document.getElementById("confirm-password");
+console.log(number);
+button.addEventListener("click", (e) => {
+  if (!number.checkValidity()) {
+    number.style.border = "3px solid red";
+    document.getElementById("number-error").innerText =
+      "Enter a valid number!!";
+  } else {
+    number.style.border = "3px solid white";
+    document.getElementById("number-error").innerText = "";
+  }
+  if (password1.value !== password2.value) {
+    password1.style.border = "3px solid red";
+    password2.style.border = "3px solid red";
+    document.getElementById("password-error").innerText =
+      "Password do not match";
+  } else {
+    password1.style.border = "3px solid white";
+    password2.style.border = "3px solid white";
+    document.getElementById("password-error").innerText = "";
+  }
+});
